1e9e552c63bbe752b0a3373be083a2342fad48d7
brndntno <145706773+brndntno@users.noreply.github.com>
2024-12-05 13:14:52 -0500

    Add files via upload

--- Day3.java
+++ Day3.java
@@ -1,3 +1,4 @@
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
@@ -5,24 +6,33 @@ import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import static java.lang.StringUTF16.charAt;
-
 public class Day3 {
     public static void main(String[] args) {
         ArrayList<String> fileData = getFileData("src/Day3Input.txt");
         String gibberish = fileData.get(0);
         ArrayList<String> allMatches = new ArrayList<>();
-        String regex = "mul\\([1-99],[1-99]\\)";
+
+        String regex = "mul\\([0-9]{1,3},[0-9]{1,3}\\)";
         Matcher m = Pattern.compile(regex).matcher(gibberish);
+
         while (m.find()) {
             allMatches.add(m.group());
         }
+
         System.out.println(allMatches);
+
+        int sum = 0;
+
         for (int i = 0; i < allMatches.size(); i++) {
-            for (int j = 0; j < allMatches.get(i).length(); j++) {
-                if (charAt(j).is)
-            }
+            String[] splitSample = allMatches.get(i).split(",");
+            int num = Integer.parseInt(splitSample[0].substring(4));
+            int index = splitSample[1].indexOf(")");
+            int num2 = Integer.parseInt(splitSample[1].substring(0, index));
+            sum += num * num2;
         }
+
+        System.out.println(sum);
+
     }
 
     public static ArrayList<String> getFileData(String fileName) {
