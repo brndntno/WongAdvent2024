46271e96f1bff9faf04b10d8992aaac992aca5b9
brndntno <145706773+brndntno@users.noreply.github.com>
2024-12-11 13:17:54 -0500

    Add files via upload

--- Day6.java
+++ Day6.java
@@ -6,8 +6,54 @@ import java.util.Scanner;
 public class Day6 {
     public static void main(String[] args) {
         ArrayList<String> fileData = getFileData("src/Day6Input.txt");
+        int rows = fileData.size();
+        int columns = fileData.get(0).length();
+        String[][] puzzle = new String[rows][columns];
+        int guardRow = 0;
+        int guardColumn = 0;
         String obstruct = "#";
+        for (int r = 0; r < puzzle.length; r++) {
+            for (int c = 0; c < puzzle[0].length; c++) {
+                if (puzzle[r][c].equals("^") || puzzle[r][c].equals("v")) {
+                    guardRow = r;
+                    guardColumn = c;
+                }
+                if (puzzle[r][c].equals(">") || puzzle[r][c].equals("<")) {
+                    guardRow = r;
+                    guardColumn = c;
+                }
+            }
+        }
+        int r = 0;
+        int c = 0;
+        while (rowInBounds(r,puzzle) && columnInBounds(c,puzzle)) {
+            if (puzzle[guardRow][guardColumn].equals("^")) {
+                
+            }
+            if (puzzle[guardRow][guardColumn].equals("v")) {
 
+            }
+            if (puzzle[guardRow][guardColumn].equals(">")) {
+
+            }
+            if (puzzle[guardRow][guardColumn].equals("<")) {
+
+            }
+        }
+    }
+
+    public static boolean rowInBounds(int row, String[][] puzzle) {
+        if (row >= 0 && row < puzzle.length) {
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean columnInBounds(int column, String[][] puzzle) {
+        if (column >= 0 && column < puzzle[0].length) {
+            return true;
+        }
+        return false;
     }
 
     public static ArrayList<String> getFileData(String fileName) {
